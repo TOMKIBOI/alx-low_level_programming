4b1dbab8bf6934ac52205abfe3af5bcfadb1141f
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-13 16:57:30 -0700

    all

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * this is the entry point
+ * tom is the author
+ */
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art, );
+	return(0);
+}	
