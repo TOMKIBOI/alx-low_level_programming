82071f5918425e7ff29f749acd3f8ca01919263d
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-16 13:25:13 -0700

    four five cor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- * main-entry-point
- * Retun: 0
+ * main-entry point
+ * Return:prints the statement
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*putsworks like prinf*/
 	return (0);
-	/*Returns:always retur 0 when successfull*/
 }
