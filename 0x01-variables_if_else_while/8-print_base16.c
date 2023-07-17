9ab6ea06407f453c474997b91310f88a12819725
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-17 15:26:40 -0700

    print hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print base 16
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	char d;
+
+	for (a = 0; a < 10; a++)
+		putchar((i % 10) + '0');
+	for (d = 97; d <= 102; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
