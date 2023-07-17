547c85dba4a50b5d4e3025fe88c5f25dddf3c46b
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-17 14:58:19 -0700

    print slphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,18 +11,19 @@
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	ld = n % 10;
 
 	printf("Last digit of %d is ", n);
 
-	if (last_digit > 5)
-		printf("%d and is greater than 5\n", last_digit);
-	else if (last_digit == 0)
-		printf("%d and is 0\n", last_digit);
+	if (ld > 5)
+		printf("%d and is greater than 5\n", ld);
+	else if (ld == 0)
+		printf("%d and is 0\n", ld);
 	else
-		printf("%d and is less than 6 and not 0\n", last_digit);
+		printf("%d and is less than 6 and not 0\n", ld);
 	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 97; d <= 122; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}}
