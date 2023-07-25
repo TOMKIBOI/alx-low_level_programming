d43ce8aa1745e7ead01f756415b3e91f782cc64e
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-25 14:03:57 -0700

    debugginf task 2 and 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,6 +7,6 @@
 
 void _puts(char *str)
 {
-	puts(str '\n')
+	puts(str'\n');
 
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
