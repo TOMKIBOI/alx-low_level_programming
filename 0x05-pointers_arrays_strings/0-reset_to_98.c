4756906d0d5d091870fb8ddb152a7a1ca927d7a9
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-25 13:11:20 -0700

    optional task pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98-function prototype
+ * @n: interger
+ * Return: always 0 when successfull
+ */
+{
+	void reset_to_98(int *n);
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 welcome to c programming
 variables
 0x03-debugging
-0x04-more_functions_nested_loops
+0x04-more_functions_nested_loop
+0x05. C - Pointers, arrays and strings
