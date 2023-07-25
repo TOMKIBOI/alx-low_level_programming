7eaf4a3a40b2d93b80c4b215a217e2ed00ddaac1
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-25 13:26:26 -0700

    task 0 debugging and task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * Return: always 0 when successfull
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,14 @@
 #include "main.h"
+/**
+ * swap_int- swaps the value of two intergers
+ * @a: first interger
+ * @b: second interger
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
