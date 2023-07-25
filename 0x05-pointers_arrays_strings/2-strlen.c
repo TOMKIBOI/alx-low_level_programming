f66ae941bcb7ed4cb92c50219870c88988a41bac
TOMKIBOI <tomkiboi@yahoo.com>
2023-07-25 13:41:00 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+ * int _strlen- finds the length of a string
+ * @s: input string
+ * Return: returns the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
+
