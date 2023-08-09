6dec0bb180167acae6d61d92212acad59a2fe080
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-09 15:35:05 -0700

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_puts- prints a string with a new line
+ *@str: input string
+ *
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - returns absolute value
+ *
+ *@n: argument prototype to convert to absolute value
+ *
+ *Return: absolute value n
+ */
+
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
