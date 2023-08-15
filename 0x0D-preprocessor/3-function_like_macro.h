9d68ef7ca06bee6898e049143198fff8cb679326
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-15 14:33:32 -0700

    C preprocessor CPP

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _FUNCTION_LIKE_
+#define _FUNCTION_LIKE_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_
+#define _SUM_LIKE_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
