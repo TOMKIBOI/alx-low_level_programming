ec8d79c0c7970fa01d0750c42fd67cf3330633ef
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-18 05:47:46 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,40 +6,45 @@
  * print_all - prints anything
  * @format: data type format
  */
+
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
-	char *tom = "", *it;
-	va_list prany;
+	int i = 0;
+	char *str, *sep = "";
 
-	va_start(prany, format);
-	while (format && format[i])
+	va_list lst;
+
+	va_start(lst, format);
+
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%s%c", tom, (char)va_arg(prany, int));
-				break;
-			case 'i':
-				printf("%s%d", tom, va_arg(prany, int));
-				break;
-			case 'f':
-				printf("%s%f", tom, va_arg(prany, double));
-				break;
-			case 's':
-				it = va_arg(prany, char*);
-				if (!it)
-				{
-					it = "(nil)";
-				}
-				printf("%s%s", tom, va_arg(prany, char*));
-				break;
-			default:
-				i++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(lst, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(lst, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(lst, double));
+					break;
+				case 's':
+					str = va_arg(lst, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		tom = ", ";
-		i++;
 	}
-	printf("\n"), va_end(prany);
+
+	printf("\n"), va_end(lst);
 }
