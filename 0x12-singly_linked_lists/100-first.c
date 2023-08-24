baed2f7324fdc4711b5286ac8298bac8c250fd4c
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-24 15:28:12 -0700

    print message

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+
+void printMessage(void) __attribute__ ((constructor));
+
+/**
+ * printMessage - prints the output message before the main
+ * function is xecuted
+ */
+
+void printMessage(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
