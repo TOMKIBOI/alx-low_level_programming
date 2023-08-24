ce21ad4fb97beec54782adbf8c3d7c925b1cc707
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-24 14:02:08 -0700

    return no. of elem in a linked lis

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,22 @@
+#include <stdlib.h>
 #include "lists.h"
+
+/**
+ * list_len - elements in a linked list
+ * @h: pointer to a list
+ *
+ * Return: returns elemets count
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elem_ count = 0;
+
+	while (h != NULL)
+	{
+		elem_count++;
+		h = h->next;
+	}
+
+	return (elem_count);
+}
