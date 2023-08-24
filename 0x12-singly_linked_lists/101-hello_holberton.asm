4badd13fd7316dc159436589edac8d3ea23bfe00
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-24 15:56:10 -0700

    error

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,13 @@
 section .data
-    hello db "Hello, Holberton",0
-    format db "%s",10,0
+    msg db "Hello, Holberton",0
+    fmt db "%s",10,0
 
 section .text
     global main
     extern printf
 
 main:
-    mov esi, hello
-    mov edi, format
+    mov esi, msg
+    mov edi, fmt
     xor eax, eax
     call printf
-    ret
