8822ea386aeb725875a4840651463c5cac8c7d4c
TOMKIBOI <tomkiboi@yahoo.com>
2023-08-31 14:29:00 -0700

    bit manupulation files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* binary_to_uint - converts a binary number to an unsigned int.
+* @b: *p to a string of 0 and 1 chars
+* Return: returns converted number or 0 if it fails.
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted_num = 0;
+	int x;
+
+	if (b == NULL)
+		return (0);
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+
+		converted_num = converted_num * 2 + (b[x] - '0');
+	}
+	return (converted_num);
+}
