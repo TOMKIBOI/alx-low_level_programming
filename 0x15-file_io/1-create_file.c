95a967960db8c06835018c84fd0425c590a6b6af
TOMKIBOI <tomkiboi@yahoo.com>
2023-09-07 13:14:57 -0700

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,36 @@
-#include
+#include "main.h"
+
+/**
+ * create_file - function prototype that creats a file.
+ * @filename: name of file to create.
+ * @text_content: text to write inside the file.
+ *
+ * Return: 1 if text_content was appened, -1 otherwise.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int filed;
+
+	if (filename == NULL)
+		return (-1);
+
+	filed = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (filed == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		ssize_t write_result = write(filed, text_content, strlen(text_content));
+
+		if (write_result == -1)
+		{
+			close(filed);
+			return (-1);
+		}
+	}
+
+	close(filed);
+	return (1);
+}
