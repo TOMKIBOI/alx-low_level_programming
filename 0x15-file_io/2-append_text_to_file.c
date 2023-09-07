a68fa4b60a9f5cace92c9ebe5a1ef7af8822cdd2
TOMKIBOI <tomkiboi@yahoo.com>
2023-09-07 13:43:52 -0700

    fix error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,28 +9,44 @@
  * Return: 1 if text_content was appened, -1 when unsuccessful.
  */
 
+/**
+* append_text_to_file - Appends text at the end of a file.
+* @filename: The name of the file.
+* @text_content: The NULL terminated string to add at the end of the file.
+*
+* Return: 1 on success and -1 on failure.
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int reslt;
-	FILE *file;
+	int file_len = 0, filed;
+	ssize_t bytes_Written;
 
-	if (filename == NULL || text_content == NULL)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
-	file = fopen(filename, "a");
 
-	if (file == NULL)
-	{
+	filed = open(filename, O_WRONLY | O_APPEND);
+
+	if (filed == -1)
 		return (-1);
-	}
-	reslt = fprintf(file, "%s", text_content);
 
-	fclose(file);
-
-	if (reslt < 0)
+	if (text_content != NULL)
 	{
-		return (-1);
+
+		while (text_content[file_len] != '\0')
+		{
+			file_len++;
+		}
+
+
+		bytes_Written = write(filed, text_content, file_len);
+
+
+		if (bytes_Written == -1)
+		{
+			return (-1);
+		}
 	}
+
+	close(filed);
 	return (1);
 }
