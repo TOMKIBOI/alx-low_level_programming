88c6077e501b153b9b634e10cc3ed523f7ec4f95
TOMKIBOI <tomkiboi@yahoo.com>
2023-09-19 19:56:09 -0700

    update function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,4 +11,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_exit(int code, const char *message);
+void print_elf_header_info(unsigned char *elf_header);
 #endif
