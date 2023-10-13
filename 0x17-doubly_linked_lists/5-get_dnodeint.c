9e2186c521161aad9f020e147e55ce1cc742dd4e
TOMKIBOI <tomkiboi@yahoo.com>
2023-10-13 12:16:36 -0700

    fixed error

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
 * get_dnodeint_at_index - Get the nth node of a dlistint_t list
 * @head: A pointer to the head of the list
-* @index: The index of the node to retrieve 
+* @index: The index of the node to retrieve
 * Return: points the nth node
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
