8796bb51c01fba4ed90a54d0da749cadff5b1bd9
TOMKIBOI <tomkiboi@yahoo.com>
2023-11-08 18:06:17 -0800

    whitespace

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,7 +3,7 @@
 /**
 * hash_table_get - Retrieves a value associated with a key
 * @ht: The hash table you looking
-* @key: The key you 
+* @key: The key you
 * Return: The value associated with the element
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
